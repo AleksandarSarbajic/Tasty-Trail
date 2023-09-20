c21bff302c0a7554b448e8c48ab35e2aa9e1fed7
Aleksandar <sleasarbajic@gmail.com>
2023-09-20 19:09:21 +0200

    css

--- src/routes/CartPage.jsx
+++ src/routes/CartPage.jsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
 import ContainerCart from "../components/cart/cartpage/Container";
+import useDocumentTitle from "../customhooks/useDocumentTitle";
 
 export default function CartPage() {
+  useDocumentTitle("Discover a Restaurant or Markets | TastyTrail");
+  useEffect(() => {
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }, []);
+
   return <ContainerCart />;
 }
